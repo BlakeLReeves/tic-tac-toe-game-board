b8875e1d824ddecf8c39c5c019575eadc45bc3e1
blakereeves19 <blake.reeves19@gmail.com>
2018-11-18 00:44:57 -0600

    Added functions

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
+let currentTurn = 0;
+let movesMade = 0;
+let gameover = false;
 let squares = document.querySelectorAll('.square');
 
-squares.forEach(function(square) {
-    square.addEventListener("click", cellClicked);
+squares.forEach(function (square) {
+    square.addEventListener("click", squareClicked);
 });
 
-function cellClicked(e) {
-    alert(e.target.textContent);
-}
+function squareClicked(e) {
+    if (e.target.textContent = '') {
+        e.target.textContent = squareOutput(currentTurn);
+    }
+};
+
+function squareOutput(currentTurn) {
+    if (gameover == false) {
+        if (currentTurn % 2 == 0) {
+            return 'X';
+        } else if (currentTurn % 2 == 1) {
+            return 'O';
+        } else {
+            alert('Error!');
+        };
+    };
+};
